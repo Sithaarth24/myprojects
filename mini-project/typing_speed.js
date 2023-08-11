30a26d66f4ec2a7de30654827f28aeb901ff630e
Sithaarth M <122381748+Sithaarth24@users.noreply.github.com>
2023-08-11 20:13:01 +0530

    Update typing_speed.js

--- mini-project/typing_speed.js
+++ mini-project/typing_speed.js
@@ -1,36 +1,105 @@
 const random_txt_url = 'https://api.quotable.io/random';
+const tell = document.getElementById("tell");
 const display=document.getElementById("display_txt");
 const inp_text = document.getElementById("get_input");
+const time = document.getElementById('time');
+const speed = document.getElementById('speed');
+const acc = document.getElementById('accuracy');
+let int = null;
+let c=0,w=0,tt=0;
+let [milliseconds,seconds,minutes] = [0,60,0];
 function get_text()
 {
     return fetch(random_txt_url)
         .then( res=> res.json())
         .then(data=> data.content)
-   
 }
-let i=0
+
+document.getElementById("get_input").disabled = true;
+
+function min(t)
+{
+    tt=t
+    document.getElementById("get_input").disabled = false;
+    display.style.color = "white";
+    document.getElementById('b1').disabled = true;
+    document.getElementById('b2').disabled = true;
+    document.getElementById('b3').disabled = true;
+    tell.innerHTML = "Start!!"
+    time.innerHTML = ''
+    minutes = t-1
+    inp_text.focus();
+    start_time()
+}
+
+
+
+let i=0;
 inp_text.addEventListener('input',() =>{
-    const key=Event.key;
-    if(key === "backspace") return
     const a = inp_text.value
     const quote_spans = display.querySelectorAll('span')
     const quote_letter = quote_spans[i].innerText
     if(quote_letter===a[a.length-1])
     {
+        c++;
         quote_spans[i].classList.add("correct");
     }
     else{
+        w++;
         quote_spans[i].classList.add("incorrect");
     }
     if(i+1==quote_spans.length)
     {
-        inp_text.value = ''
+
         get_text()
         random_txt()
         i=0
     }
-    i++;
+    else i++
 })
+
+function start_time()
+    {
+        int = setInterval(displayTimer,100);
+    }
+
+function displayTimer(){
+    milliseconds+=100;
+    if(milliseconds == 1000)
+    {
+        milliseconds = 0;
+        seconds--;
+        if(seconds == 0)
+        {
+            if(minutes == 0) 
+            {
+                validation()
+                clearInterval(int)
+            }
+            else{
+                seconds = 60;
+                minutes--;
+            }
+        }
+    }
+    let s = (seconds<10)?"0"+seconds:seconds;
+    time.innerHTML = ` ${"0" + minutes} : ${s} `;
+}
+
+function validation()
+{
+    time.style.color = "red"
+    inp_text.value = ''   
+    tell.innerHTML = "Stop!"
+    tell.style.color = "red";
+    document.getElementById("get_input").disabled = true;
+    let s = c/(tt*5.3)
+    s=s.toFixed(2)
+    speed.innerHTML = "Typing speed : " + s + " wpm";
+    let a = (c/(c+w))*100
+    a=a.toFixed(2)
+    acc.innerHTML = "Accuracy : " + a + "%"
+}
 
 function preventBackspace(e) {
     var evt = e || window.event;
@@ -59,5 +128,5 @@ async function random_txt()
         char_span.innerText = element;
         display.appendChild(char_span)
     });
-    
+    inp_text.value = ''
 }
